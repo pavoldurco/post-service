3d3beee20708ebe64d81f20fc6e0849e78add597
pavoldurco <pavol.durco@yoso.fi>
2024-02-04 22:15:37 +0100

    Initial commit

---
+++ src/test/java/sk/pelikan/post/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.pelikan.post.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
